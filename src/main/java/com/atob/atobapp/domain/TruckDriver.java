b2688cf8da444371ca3093f31fbf88c6edd611c8
beka toro <bekakikalishvili18@gmail.com>
2023-09-04 17:22:59 +0200

    order have new conect  with product_id

--- src/main/java/com/atob/atobapp/domain/TruckDriver.java
+++ src/main/java/com/atob/atobapp/domain/TruckDriver.java
@@ -1,6 +1,8 @@
 package com.atob.atobapp.domain;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -18,4 +20,8 @@ public class TruckDriver {
     private Integer Postcode;
     private String City;
     private Integer Phone;
+
+    @ManyToOne
+    @JoinColumn(name="TransportOrder_id")
+    private TransportOrder transportOrder;
 }
