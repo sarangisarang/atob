ad575c98d0e2758b7a12624bed8d505e0050b3c3
beka toro <bekakikalishvili18@gmail.com>
2023-10-05 18:25:17 +0200

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/main/java/com/atob/atobapp/controler/OrderControler.java

--- src/test/java/com/atob/atobapp/TransporOrderServiceTest.java
+++ src/test/java/com/atob/atobapp/TransporOrderServiceTest.java
@@ -1,5 +1,4 @@
 package com.atob.atobapp;
-
 import com.atob.atobapp.domain.TransportOrder;
 import com.atob.atobapp.repository.*;
 import com.atob.atobapp.service.OrderService;
@@ -8,10 +7,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import javax.xml.stream.Location;
 import java.time.LocalDate;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -32,7 +28,7 @@ public class TransporOrderServiceTest {
     @Autowired
     private OrderService orderService;
     @Test
-    public void given_order_with_all_when_has_status_Diferent_processing_can_not_change(){
+    public void given_transportorder_with_all_when_has_status_diferent_processing_can_not_change(){
         TransportOrder orders = new TransportOrder();
         orders.setOrderNo(20);
         orders.setOrderId("10");
@@ -44,8 +40,7 @@ public class TransporOrderServiceTest {
         transportOrderRepository.save(orders);
         TransportOrder neworders =  transportOrderRepository.findById("1234").orElseThrow();
         Assertions.assertNotNull(neworders);
-        Exception exception = assertThrows(Exception.class,()-> orderService.UpdateOrder(neworders.getId(),));
+        Exception exception = assertThrows(Exception.class,()-> orderService.UpdateOrder(neworders,neworders.getId()));
         assertEquals(exception.getMessage(),"Not allowed to update  order" );
     }
-
 }
